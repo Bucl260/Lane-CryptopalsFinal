859ccb48178a85ef1add9626b56059873c446acb
Bucl260 <bucl260@aa.edu>
2025-03-26 19:13:18 -0600

    Updated test and code

--- tdd.py
+++ tdd.py
@@ -1,3 +1,17 @@
 def hex_to_base64(hex_string):
    byte_data = bytes.fromhex(hex_string)
-   return ""
+   base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+   base64_string = ""
+
+   for i in range(0, len(byte_data), 3):
+        chunk = byte_data[i:i+3]
+        binary_str = ''.join(f'{byte:08b}' for byte in chunk)
+
+        while len(binary_str) % 6 != 0:
+            binary_str += '0'
+
+        for j in range(0, len(binary_str), 6):
+            index = int(binary_str[j:j+6], 2)
+            base64_string += base64_chars[index]
+        
+   return base64_string
--- test_tdd.py
+++ test_tdd.py
@@ -4,6 +4,9 @@ from tdd import hex_to_base64
 class TestHexToBase64(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(hex_to_base64(""), "")
+        
+    def test_three_bytes(self):
+        self.assertEqual(hex_to_base64("4d616e"), "TWFu")
 
 if __name__ == '__main__':
     unittest.main()
