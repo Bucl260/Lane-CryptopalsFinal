49377b4ffa6c5bd8fa52ef99b5a7cf4e0bc31916
Bucl260 <bucl260@aa.edu>
2025-03-26 19:22:31 -0600

    added challenge test

--- test_tdd.py
+++ test_tdd.py
@@ -13,6 +13,11 @@ class TestHexToBase64(unittest.TestCase):
 
     def test_two_bytes(self):
         self.assertEqual(hex_to_base64("4d61"), "TWE=")
+        
+    def test_conversion(self):
+        hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+        expected_base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+        self.assertEqual(hex_to_base64(hex_string), expected_base64)
 
 if __name__ == '__main__':
     unittest.main()
