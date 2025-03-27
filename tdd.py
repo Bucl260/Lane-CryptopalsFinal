6775cfa026c5271de68c8ed9b84e41a4e5580d2c
Bucl260 <bucl260@aa.edu>
2025-03-26 19:21:42 -0600

    refactor

--- tdd.py
+++ tdd.py
@@ -1,19 +1,18 @@
 def hex_to_base64(hex_string):
    byte_data = bytes.fromhex(hex_string)
    base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-   base64_string = ""
+   base64_string = []
 
    for i in range(0, len(byte_data), 3):
         chunk = byte_data[i:i+3]
         binary_str = ''.join(f'{byte:08b}' for byte in chunk)
-
         binary_str = binary_str.ljust((len(binary_str) + 5) // 6 * 6, '0')
 
         for j in range(0, len(binary_str), 6):
             index = int(binary_str[j:j+6], 2)
-            base64_string += base64_chars[index]
+            base64_string.append(base64_chars[index])
         
    padding_length = (3 - len(byte_data) % 3) % 3
-   base64_string += '=' * padding_length
+   base64_string.extend('=' * padding_length)
         
-   return base64_string
+   return ''.join(base64_string)
